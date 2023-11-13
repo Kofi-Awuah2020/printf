742aac2663f0e908a27370e331a171b544172ae6
Love <honnylove77@gmail.com>
2023-11-13 08:46:07 -0800

    main.h

--- main.h
+++ main.h
@@ -9,7 +9,7 @@ int _printf(const char *format, ...);
 int print_char(va_list val);
 int print_str(va_list val);
 int _strlen(char *s);
-int _strlenc(const char *s)
+int _strlenc(const char *s);
 int print_percent(void);
 
 #endif
