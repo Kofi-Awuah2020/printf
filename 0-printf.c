a9eb517789b6c16f20515cac5d6b77d86489c44c
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-11-14 16:10:21 +0000

    Updated 0-printf

--- 0-printf.c
+++ 0-printf.c
@@ -1,39 +1,62 @@
 #include "main.h"
 /**
-*	_printf -  produces output according to a format.
-*	@format: function parameter
-*	Return: 0 success, -1 failure
-*/
+ * _printf -  produces output according to a format.
+ * @format: function parameter
+ * Return: 0 success, -1 failure
+ */
 
 int _printf(const char *format, ...)
 {
-	int char_print = 0;
-	va_list args;
+    if (format == NULL)
+        return (-1);
 
-	if (format == NULL)
-	return (-1);
+    int i = 0;
+    int len = 0;
+    va_list args;
 
-	va_start(args, format);
+    va_start(args, format);
+    
+    while (format[i] != '\0')
+    {
+        if (format[i] != '%')
+        {
+            write(1, &format[i], 1);
+            len++;
+        }
+        else
+        {
+            i++; /* check next character */
+            if (format[i] == 'c')
+            {
+                char c = va_arg(args, int);
+                
+				write(1, &c, 1);
+                len++;
+            }
+            else if (format[i] == 's')
+            {
+                char *str = va_arg(args, char *);
+                
+				if (str != NULL)
+                {
+                    write(1, str, strlen(str));
+                    len += strlen(str);
+                }
+                else
+                {
+                    write(1, "(null)", 6); /* Handle NULL string */
+                    len += 6;
+                }
+            }
+            else if (format[i] == '%')
+            {
+                write(1, "%", 1);
+                len++;
+            }
+        }
+        i++;
+    }
 
-	while (*format)
-	{
-	if (*format != '%' || (*(format + 1) == '%' && format++))
-	{
-		write(1, format, 1);
-		char_print++;
-	}
-	else if (*format == 'c')
-	{
-		char c = va_arg(args, int);
-
-		write(1, &c, 1);
-		char_print++;
-	}
-
-	format++;
-	}
-
-	va_end(args);
-
-	return (char_print);
+    va_end(args);
+    return (len);
 }
